785f1fcab26aa146720d852779ceb10dd0d041e9
Xenomorph456 <121023268+Xenomorph456@users.noreply.github.com>
2023-11-22 11:14:18 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "9541960"
 api_hash = "17a1aa509b35564481dae98d89d0191d"
-bot_token = "6682310867:AAF2dAhbDjD_KIzWul5erokKEBOXq_-tOZo"
-sudo_groups = -1001938053139
+bot_token = "6317224996:AAG-fIs-iPGfaVmHmePIPspVYMgVpSTBAYA"
+sudo_groups = -1001577274993
