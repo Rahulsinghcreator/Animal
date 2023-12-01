ce090a4feea315f6b8b58afff0374069eb6d67fc
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-12-01 21:37:57 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = "28590119"
-api_hash = "2494557bf21e6c5152f26070aa1a97c7"
-bot_token = "6314405678:AAGVafrn8cahK7i8_9MepAZLKEmZu_mGsCQ"
-sudo_groups = -1001806613008
+api_id = "21359555"
+api_hash = "526d12e4855af175e0e3d6e22beba6aa"
+bot_token = "6930088546:AAFOkh1wlahXpi-gPaN2GozXjSLo_nSHSYg"
+sudo_groups = -1002103415478
 
